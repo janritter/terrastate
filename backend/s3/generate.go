50aa37da449455645aa7260dbd4aa63772e16d51
Jan Ritter <git@janrtr.de>
2019-03-02 11:55:11 +0100

    refactor: use generic output method instead of hardcoded output

--- backend/s3/generate.go
+++ backend/s3/generate.go
@@ -1,11 +1,7 @@
 package s3
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-
-	"github.com/fatih/color"
+	"github.com/janritter/terrastate/helper"
 )
 
 func (backend *S3Backend) GenerateStatefileForBackend(in interface{}) error {
@@ -15,38 +11,8 @@ func (backend *S3Backend) GenerateStatefileForBackend(in interface{}) error {
 		return err
 	}
 
-	fmt.Println("")
-	fmt.Println("------- Using the following values -------")
-	fmt.Println("bucket = " + stateParams.Bucket)
-	fmt.Println("dyanmodb_table = " + stateParams.DynamoDBTable)
-	fmt.Println("key = " + stateParams.Key)
-	fmt.Println("region = " + stateParams.Region)
-	fmt.Println("------------------------------------------")
+	helper.PrintStateValues(stateParams)
 
 	err = createStateFile(stateParams)
 	return err
-}
-
-func createStateFile(in stateConfig) error {
-	fileContent :=
-		`terraform {
-  backend "s3" {
-	encrypt        = true
-	bucket         = "` + in.Bucket + `"
-	region         = "` + in.Region + `"
-	key            = "` + in.Key + `"
-	dynamodb_table = "` + in.DynamoDBTable + `"
-  }
-}
-`
-
-	data := []byte(fileContent)
-	err := ioutil.WriteFile("terrastate.tf", data, 0644)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	color.Green("Successfully created terrastate.tf")
-	return nil
 }
