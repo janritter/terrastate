d2a834d9bd48b0949b3092538e66a43df0fba83f
Jan Ritter <git@janrtr.de>
2019-03-24 15:26:14 +0100

    feat: added option to automaticly remove the .terraform folder when creation a new backend config file - closes #6

--- backend/s3/generate.go
+++ backend/s3/generate.go
@@ -13,6 +13,11 @@ func (backend *S3Backend) GenerateConfigurationForBackend(in interface{}) error 
 
 	helper.PrintStateValues(stateParams)
 
+	err = helper.RemoveDotTerraformFolder(in)
+	if err != nil {
+		return err
+	}
+
 	err = createBackendConfigurationFile(stateParams)
 	return err
 }
