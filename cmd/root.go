b97ce5ac12df83caf2d744dcf3ae2bbb8519e9c7
Jan Ritter <git@janrtr.de>
2019-03-24 16:08:42 +0100

    refactor: replaced cobra info with terrastate info

--- cmd/root.go
+++ cmd/root.go
@@ -44,13 +44,7 @@ var varFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "terrastate",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Tool to manage multiple state backends in Terraform - Allows Multi account setups",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
