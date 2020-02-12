e6f1fb0c396ad9e6f5881c0c49a6d5b70a9985bd
Jan Ritter <git@janrtr.de>
2020-02-12 23:36:44 +0100

    refactor: replaced example description of version endpoint

--- cmd/version.go
+++ cmd/version.go
@@ -33,13 +33,8 @@ var buildTime string
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Includes version information about terrastate",
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("Version - %s \n", version)
 		fmt.Printf("Git SHA - %s \n", gitSha)
