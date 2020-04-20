cb1cc13e0e6262b69c2ebf1d9f248260faff96e9
Jan Ritter <git@janrtr.de>
2020-04-20 21:24:48 +0200

    feat: added plan to call terrastate, terraform init and terraform plan with a single command

---
+++ cmd/plan.go
@@ -0,0 +1,55 @@
+// Copyright © 2020 Jan Ritter <git@janrtr.de>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+// planCmd represents the plan command
+var planCmd = &cobra.Command{
+	Use:   "plan",
+	Short: "Run terraform plan through terrastate, also executes terrastate and terraform init",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Terrastate
+		fmt.Printf("Running terrastate \n\n")
+		rootCmd.Run(cmd, args)
+
+		// Terraform init
+		if err := getTerraformExecCmdForSubcommand("init", varFile).Run(); err != nil {
+			color.Red("terraform init returned the following error code: " + err.Error())
+			return
+		}
+
+		// Terraform plan
+		if err := getTerraformExecCmdForSubcommand("plan", varFile).Run(); err != nil {
+			color.Red("terraform plan returned the following error code: " + err.Error())
+			return
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(planCmd)
+}
