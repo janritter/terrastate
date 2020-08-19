6218b9ac3e8d8411d1c33715469461654ceb1984
Jan Ritter <jan.ritter@trustedshops.de>
2020-08-19 12:09:19 +0200

    feat: added support for terraform init command

---
+++ cmd/init.go
@@ -0,0 +1,44 @@
+/*
+Copyright © 2020 Jan Ritter <git@janrtr.de>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Run terraform init through terrastate",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Terrastate
+		fmt.Printf("Running terrastate \n\n")
+		rootCmd.Run(cmd, args)
+
+		// Terraform init
+		if err := getTerraformExecCmdForSubcommand("init", varFile).Run(); err != nil {
+			color.Red("terraform init returned the following error code: " + err.Error())
+			return
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
