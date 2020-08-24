7efc6ef10d101a08cd01590079b7229c5b6bcbcb
Jan Ritter <git@janrtr.de>
2020-08-24 23:51:52 +0200

    test: adapted s3 test

--- backend/s3/s3_test.go
+++ backend/s3/s3_test.go
@@ -7,8 +7,14 @@ import (
 )
 
 func TestNewS3Backend(t *testing.T) {
-	backend := NewS3Backend(nil)
+	testMap := make(map[string]interface{})
+	testMap["state_bucket"] = "test_bucket"
+	testMap["state_dynamodb_table"] = "test_dynamodb_table"
 
-	assert.Empty(t, backend, "Expected to be empty")
+	backend := NewS3Backend(testMap)
+
+	assert.NotEmpty(t, backend, "Expected to be not empty")
 	assert.NotNil(t, backend, "Expected not to be nil")
+	assert.NotNil(t, backend.VarParser, "Expected parser not to be nil")
+	assert.Equal(t, testMap, backend.VarFile)
 }
