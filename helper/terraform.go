1bf1a580596d12d1c96c5e0f32dff0176f47d3ad
Jan Ritter <git@janrtr.de>
2020-08-24 23:45:00 +0200

    perf(rework): replaced errors with os.exit

--- helper/terraform.go
+++ helper/terraform.go
@@ -7,25 +7,24 @@ import (
 	"github.com/janritter/terrastate/helper/parser"
 )
 
-func RemoveDotTerraformFolder(in interface{}) error {
+func RemoveDotTerraformFolder(in interface{}) {
 	varParser := parser.NewParser(in)
 	shouldRemove, valueSet, err := varParser.GetBackendParameterBool("state_auto_remove_old", true)
 	if err != nil {
-		return err
+		color.Red(err.Error())
+		os.Exit(1)
 	}
 
 	if valueSet == false || shouldRemove == false {
 		color.Blue("Skipping removing of .terraform/terraform.tfstate")
-		return nil
 	}
 
 	if valueSet == true && shouldRemove == true {
 		err = os.RemoveAll(".terraform/terraform.tfstate")
 		if err != nil {
 			color.Red(err.Error())
-			return err
+			os.Exit(1)
 		}
 		color.Green("Removed .terraform/terraform.tfstate")
 	}
-	return nil
 }
