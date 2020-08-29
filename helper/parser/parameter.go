9d845e40dbb6e800234ae3638474ec9a39c6077d
Jan Ritter <git@janrtr.de>
2020-08-29 09:18:12 +0200

    refactor: removed unsued functions and replaced os.Exit with custom osExit to make it testable

--- helper/parser/parameter.go
+++ helper/parser/parameter.go
@@ -3,7 +3,6 @@ package parser
 import (
 	"errors"
 	"fmt"
-	"os"
 	"reflect"
 
 	"github.com/fatih/color"
@@ -28,6 +27,7 @@ func (parser *Parser) Gather(stateFileAttributes []*types.StateFileAttribute) {
 			if attribute.Given {
 				if reflect.TypeOf(mapped[attribute.VarKey]).String() != attribute.ExpectedType {
 					err := errors.New("Expected " + attribute.VarKey + " to be " + attribute.ExpectedType + ", was " + reflect.TypeOf(mapped[attribute.VarKey]).String())
+					attribute.Given = false
 					color.Red(err.Error())
 					errorRun = true
 				} else if reflect.TypeOf(mapped[attribute.VarKey]).String() == "string" {
@@ -39,13 +39,13 @@ func (parser *Parser) Gather(stateFileAttributes []*types.StateFileAttribute) {
 		}
 
 		if errorRun {
-			os.Exit(1)
+			osExit(1)
 		}
 
 	default:
 		err := errors.New("Unknown var-file format")
 		color.Red(err.Error())
-		os.Exit(1)
+		osExit(1)
 	}
 }
 
@@ -76,76 +76,12 @@ func (parser *Parser) GatherTerrastateVariables(terrastateAttribute map[string]*
 		}
 
 		if errorRun {
-			os.Exit(1)
+			osExit(1)
 		}
 
 	default:
 		err := errors.New("Unknown var-file format")
 		color.Red(err.Error())
-		os.Exit(1)
-	}
-}
-
-func (parser *Parser) GetBackendParameterString(key string, optional bool) (string, bool, error) {
-	iface, valueSet, err := parser.getSingleBackendParameterInterface(key, optional, "string")
-	if err != nil {
-		return "", valueSet, err
-	}
-	if valueSet == false {
-		return "", valueSet, nil
-	}
-	return iface.(string), valueSet, nil
-}
-
-func (parser *Parser) GetBackendParameterInt(key string, optional bool) (int, bool, error) {
-	iface, valueSet, err := parser.getSingleBackendParameterInterface(key, optional, "int")
-	if err != nil {
-		return 0, valueSet, err
-	}
-	if valueSet == false {
-		return 0, valueSet, nil
-	}
-	return iface.(int), valueSet, nil
-}
-
-// Return values are: an error, the parsed bool value and if the value in the var-file was set.
-func (parser *Parser) GetBackendParameterBool(key string, optional bool) (bool, bool, error) {
-	iface, valueSet, err := parser.getSingleBackendParameterInterface(key, optional, "bool")
-	if err != nil {
-		return false, valueSet, err
-	}
-	if valueSet == false {
-		return false, valueSet, nil
-	}
-	return iface.(bool), valueSet, nil
-}
-
-func (parser *Parser) getSingleBackendParameterInterface(key string, optional bool, expectedType string) (interface{}, bool, error) {
-	switch parser.VarFileContent.(type) {
-	case map[string]interface{}:
-		mapped := parser.VarFileContent.(map[string]interface{})
-
-		valueSet := mapped[key] != nil
-
-		if valueSet == false {
-			if optional == false {
-				err := errors.New(key + " must be defined, was not found in var-file")
-				color.Red(err.Error())
-				return nil, valueSet, err
-			}
-			return nil, valueSet, nil
-		}
-
-		if reflect.TypeOf(mapped[key]).String() != expectedType {
-			err := errors.New("Expected " + key + " to be " + expectedType + ", was " + reflect.TypeOf(mapped[key]).String())
-			color.Red(err.Error())
-			return nil, false, err
-		}
-		return mapped[key], valueSet, nil
-
-	default:
-		err := errors.New("Unknown var-file format")
-		color.Red(err.Error())
-		return nil, false, err
+		osExit(1)
 	}
 }
